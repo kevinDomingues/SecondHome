292da265d7babe8206a915b14f2c77b3cf8aa49d
brunopereira03 <77056372+brunopereira03@users.noreply.github.com>
2022-06-10 23:51:49 +0100

    favorites changes

--- app/src/main/java/ipvc/estg/secondhome/Favorites.kt
+++ app/src/main/java/ipvc/estg/secondhome/Favorites.kt
@@ -3,9 +3,9 @@ package ipvc.estg.secondhome
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Spinner
+import android.widget.RelativeLayout
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 
 class Favorites : Fragment(R.layout.fragment_favorites) {
@@ -14,9 +14,15 @@ class Favorites : Fragment(R.layout.fragment_favorites) {
     super.onViewCreated(view, savedInstanceState)
 
     val filterButton = getView()?.findViewById<Button>(R.id.filter)
-    val spinner = getView()?.findViewById<Spinner>(R.id.filterSpinner)
-    filterButton?.setOnClickListener {
-
+    val filterDropdown = getView()?.findViewById<RelativeLayout>(R.id.includeSpinner)
+    filterDropdown?.setVisibility(View.GONE)
+    filterButton?.setOnClickListener{
+      if(filterDropdown!!.isVisible){
+        filterDropdown?.setVisibility(View.GONE)
+      }
+      else{
+        filterDropdown?.setVisibility(View.VISIBLE)
+      }
     }
 
   }
